d11b32b02f4416c0df80ff29e8fc825cc0de7bee
windows.zhang <leitao.zhang@phpstu.com>
2021-08-09 16:56:08 +0800

    完善淘礼金和订单结果

--- tbopensdk/response/tbkdgvegastljcreate/tbkdgvegastljcreate.go
+++ tbopensdk/response/tbkdgvegastljcreate/tbkdgvegastljcreate.go
@@ -4,13 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/mimicode/tksdk/tbopensdk/response"
-	tbkdgvegastljcreateresult2 "github.com/mimicode/tksdk/tbopensdk/response/tbkdgvegastljcreateresult"
 )
 
 //taobao.tbk.dg.vegas.tlj.create( 淘宝客-推广者-淘礼金创建 )
 type Response struct {
 	response.TopResponse
-	TbkDgVegasTljCreateResponse tbkdgvegastljcreateresult2.TbkDgVegasTljCreateResponse `json:"tbk_dg_vegas_tlj_create_response"`
+	CreateResponse CreateResponse `json:"tbk_dg_vegas_tlj_create_response"`
 }
 
 //解析输出结果
@@ -23,19 +22,37 @@ func (t *Response) WrapResult(result string) {
 		t.ErrorResponse.Code = -1
 		t.ErrorResponse.Msg = unmarshal.Error()
 	} else {
-		if t.TbkDgVegasTljCreateResponse.Result.MsgCode != "" {
+		if t.CreateResponse.Result.MsgCode != "" {
 			t.ErrorResponse.Code = -2
-			t.ErrorResponse.Msg = fmt.Sprintf("MsgCode:%s MsgInfo:%s", t.TbkDgVegasTljCreateResponse.Result.MsgCode, t.TbkDgVegasTljCreateResponse.Result.MsgInfo)
-			t.ErrorResponse.SubCode = t.TbkDgVegasTljCreateResponse.Result.MsgCode
-			t.ErrorResponse.SubMsg = t.TbkDgVegasTljCreateResponse.Result.MsgInfo
-			t.ErrorResponse.RequestID = t.TbkDgVegasTljCreateResponse.RequestID
-		} else if t.TbkDgVegasTljCreateResponse.Result.Model == nil || t.TbkDgVegasTljCreateResponse.Result.Model.SendURL == "" {
+			t.ErrorResponse.Msg = fmt.Sprintf("MsgCode:%s MsgInfo:%s", t.CreateResponse.Result.MsgCode, t.CreateResponse.Result.MsgInfo)
+			t.ErrorResponse.SubCode = t.CreateResponse.Result.MsgCode
+			t.ErrorResponse.SubMsg = t.CreateResponse.Result.MsgInfo
+			t.ErrorResponse.RequestID = t.CreateResponse.RequestID
+		} else if t.CreateResponse.Result.Model == nil || t.CreateResponse.Result.Model.SendURL == "" {
 			t.ErrorResponse.Code = -3
 			t.ErrorResponse.Msg = "用户无权创建淘礼金"
 			t.ErrorResponse.SubCode = "用户无权创建淘礼金"
 			t.ErrorResponse.SubMsg = "用户无权创建淘礼金"
-			t.ErrorResponse.RequestID = t.TbkDgVegasTljCreateResponse.RequestID
+			t.ErrorResponse.RequestID = t.CreateResponse.RequestID
 		}
 	}
 
+}
+
+type CreateResponse struct {
+	Result    Result `json:"result"`
+	RequestID string `json:"request_id"`
+}
+
+type Result struct {
+	MsgCode string `json:"msg_code"`
+	MsgInfo string `json:"msg_info"`
+	Success bool   `json:"success"`
+	Model   *Model `json:"model"`
+}
+
+type Model struct {
+	RightsID  string `json:"rights_id"`
+	SendURL   string `json:"send_url"`
+	VegasCode string `json:"vegas_code"`
 }
--- tbopensdk/response/tbkscorderdetailsget/tbkscorderdetailsget.go
+++ tbopensdk/response/tbkscorderdetailsget/tbkscorderdetailsget.go
@@ -8,6 +8,7 @@ import (
 //taobao.tbk.sc.order.details.get( 淘宝客【服务商】所有订单查询 )
 type Response struct {
 	response.TopResponse
+	TbkScOrderDetailsGetResponse Result `json:"tbk_sc_order_details_get_response"`
 }
 
 //解析输出结果
@@ -20,4 +21,87 @@ func (t *Response) WrapResult(result string) {
 		t.ErrorResponse.Code = -1
 		t.ErrorResponse.Msg = unmarshal.Error()
 	}
+}
+
+type Result struct {
+	Data      Data   `json:"data"`
+	RequestID string `json:"request_id"`
+}
+
+type Data struct {
+	HasNext       bool    `json:"has_next"`
+	HasPre        bool    `json:"has_pre"`
+	PageNo        int64   `json:"page_no"`
+	PageSize      int64   `json:"page_size"`
+	PositionIndex string  `json:"position_index"`
+	Results       Results `json:"results"`
+}
+
+type Results struct {
+	PublisherOrderDto []PublisherOrderDto `json:"publisher_order_dto"`
+}
+
+type PublisherOrderDto struct {
+	AdzoneID                           int64              `json:"adzone_id"`
+	AdzoneName                         string             `json:"adzone_name"`
+	AlimamaRate                        string             `json:"alimama_rate"`
+	AlimamaShareFee                    string             `json:"alimama_share_fee"`
+	AlipayTotalPrice                   string             `json:"alipay_total_price"`
+	AppKey                             *string            `json:"app_key,omitempty"`
+	ClickTime                          string             `json:"click_time"`
+	DepositPrice                       string             `json:"deposit_price"`
+	FlowSource                         string             `json:"flow_source"`
+	IncomeRate                         string             `json:"income_rate"`
+	ItemCategoryName                   string             `json:"item_category_name"`
+	ItemID                             *int64             `json:"item_id,omitempty"`
+	ItemImg                            string             `json:"item_img"`
+	ItemLink                           *string            `json:"item_link,omitempty"`
+	ItemNum                            int64              `json:"item_num"`
+	ItemPrice                          *string            `json:"item_price,omitempty"`
+	ItemTitle                          string             `json:"item_title"`
+	OrderType                          string             `json:"order_type"`
+	PubID                              int64              `json:"pub_id"`
+	PubShareFee                        string             `json:"pub_share_fee"`
+	PubSharePreFee                     string             `json:"pub_share_pre_fee"`
+	PubShareRate                       string             `json:"pub_share_rate"`
+	RefundTag                          int64              `json:"refund_tag"`
+	SellerNick                         string             `json:"seller_nick"`
+	SellerShopTitle                    string             `json:"seller_shop_title"`
+	ServiceFeeDtoList                  *ServiceFeeDtoList `json:"service_fee_dto_list,omitempty"`
+	SiteID                             int64              `json:"site_id"`
+	SiteName                           string             `json:"site_name"`
+	SubsidyFee                         string             `json:"subsidy_fee"`
+	SubsidyRate                        string             `json:"subsidy_rate"`
+	SubsidyType                        string             `json:"subsidy_type"`
+	TBDepositTime                      string             `json:"tb_deposit_time"`
+	TBPaidTime                         string             `json:"tb_paid_time"`
+	TerminalType                       string             `json:"terminal_type"`
+	TkCommissionFeeForMediaPlatform    string             `json:"tk_commission_fee_for_media_platform"`
+	TkCommissionPreFeeForMediaPlatform string             `json:"tk_commission_pre_fee_for_media_platform"`
+	TkCommissionRateForMediaPlatform   string             `json:"tk_commission_rate_for_media_platform"`
+	TkCreateTime                       string             `json:"tk_create_time"`
+	TkDepositTime                      string             `json:"tk_deposit_time"`
+	TkOrderRole                        int64              `json:"tk_order_role"`
+	TkPaidTime                         string             `json:"tk_paid_time"`
+	TkStatus                           int64              `json:"tk_status"`
+	TkTotalRate                        string             `json:"tk_total_rate"`
+	TotalCommissionFee                 string             `json:"total_commission_fee"`
+	TotalCommissionRate                string             `json:"total_commission_rate"`
+	TradeID                            string             `json:"trade_id"`
+	TradeParentID                      string             `json:"trade_parent_id"`
+	PayPrice                           *string            `json:"pay_price,omitempty"`
+	TkEarningTime                      *string            `json:"tk_earning_time,omitempty"`
+	RelationID                         int64              `json:"relation_id"`
+	SpecialId                          int64              `json:"special_id"`
+}
+
+type ServiceFeeDtoList struct {
+	ServiceFeeDto []ServiceFeeDto `json:"service_fee_dto"`
+}
+
+type ServiceFeeDto struct {
+	SharePreFee       string `json:"share_pre_fee"`
+	ShareFee          string `json:"share_fee"`
+	ShareRelativeRate string `json:"share_relative_rate"`
+	TkShareRoleType   int64  `json:"tk_share_role_type"`
 }
