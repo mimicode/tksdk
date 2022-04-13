6781d3e8a85d721b1df3bfb5f3da0f82ebf93b44
windows.zhang <leitao.zhang@phpstu.com>
2022-04-13 11:00:13 +0800

    完善京东搜索接口返回参数 返回参数过多部分没有加入

--- jdopensdk/response/jdunionopengoodsquery/jdunionopengoodsquery.go
+++ jdopensdk/response/jdunionopengoodsquery/jdunionopengoodsquery.go
@@ -48,7 +48,134 @@ type Responce struct {
 
 // QueryResult 具体内容
 type QueryResult struct {
-	Code      int64  `json:"code"`
-	Message   string `json:"message"`
-	RequestID string `json:"requestId"`
+	Code       int64   `json:"code"`
+	Data       []Datum `json:"data"`
+	Message    string  `json:"message"`
+	RequestID  string  `json:"requestId"`
+	TotalCount int64   `json:"totalCount"`
+}
+
+type Datum struct {
+	BrandCode          string         `json:"brandCode"`
+	BrandName          string         `json:"brandName"`
+	CategoryInfo       CategoryInfo   `json:"categoryInfo"`
+	Comments           int64          `json:"comments"`
+	CommissionInfo     CommissionInfo `json:"commissionInfo"`
+	CouponInfo         CouponInfo     `json:"couponInfo"`
+	DeliveryType       int64          `json:"deliveryType"`
+	ForbidTypes        []int64        `json:"forbidTypes"`
+	GoodCommentsShare  float64        `json:"goodCommentsShare"`
+	ImageInfo          ImageInfo      `json:"imageInfo"`
+	InOrderComm30Days  float64        `json:"inOrderComm30Days"`
+	InOrderCount30Days int64          `json:"inOrderCount30Days"`
+	IsHot              int64          `json:"isHot"`
+	IsJdSale           int64          `json:"isJdSale"`
+	MaterialURL        string         `json:"materialUrl"`
+	Owner              string         `json:"owner"`
+	PinGouInfo         PinGouInfo     `json:"pinGouInfo"`
+	PriceInfo          PriceInfo      `json:"priceInfo"`
+	ShopInfo           ShopInfo       `json:"shopInfo"`
+	SkuID              int64          `json:"skuId"`
+	SkuName            string         `json:"skuName"`
+	Spuid              int64          `json:"spuid"`
+	VideoInfo          VideoInfo      `json:"videoInfo"`
+	BookInfo           BookInfo       `json:"bookInfo"`
+}
+
+type BookInfo struct {
+	Isbn string `json:"isbn"`
+}
+
+type VideoInfo struct {
+	VideoList []VideoList `json:"videoList"`
+}
+
+type VideoList struct {
+	Video Video `json:"video"`
+}
+
+type Video struct {
+	Width     int    `json:"width"`
+	High      int    `json:"high"`
+	ImageUrl  string `json:"imageUrl"`
+	VideoType int    `json:"videoType"`
+	PlayType  int    `json:"playType"`
+	Duration  int    `json:"duration"`
+	PlayUrl   string `json:"playUrl"`
+}
+
+type PinGouInfo struct {
+	PingouEndTime   int64   `json:"pingouEndTime"`
+	PingouPrice     float64 `json:"pingouPrice"`
+	PingouStartTime int64   `json:"pingouStartTime"`
+	PingouTmCount   int64   `json:"pingouTmCount"`
+	PingouURL       string  `json:"pingouUrl"`
+}
+
+type CategoryInfo struct {
+	Cid1     int64  `json:"cid1"`
+	Cid1Name string `json:"cid1Name"`
+	Cid2     int64  `json:"cid2"`
+	Cid2Name string `json:"cid2Name"`
+	Cid3     int64  `json:"cid3"`
+	Cid3Name string `json:"cid3Name"`
+}
+
+type CommissionInfo struct {
+	Commission          float64 `json:"commission"`
+	CommissionShare     float64 `json:"commissionShare"`
+	CouponCommission    float64 `json:"couponCommission"`
+	EndTime             int64   `json:"endTime"`
+	IsLock              int64   `json:"isLock"`
+	PlusCommissionShare float64 `json:"plusCommissionShare"`
+	StartTime           int64   `json:"startTime"`
+}
+
+type CouponInfo struct {
+	CouponList []CouponList `json:"couponList"`
+}
+
+type CouponList struct {
+	BindType      int64   `json:"bindType"`
+	Discount      float64 `json:"discount"`
+	GetEndTime    int64   `json:"getEndTime"`
+	GetStartTime  int64   `json:"getStartTime"`
+	HotValue      *int64  `json:"hotValue,omitempty"`
+	IsBest        int64   `json:"isBest"`
+	IsInputCoupon int64   `json:"isInputCoupon"`
+	Link          string  `json:"link"`
+	PlatformType  int64   `json:"platformType"`
+	Quota         float64 `json:"quota"`
+	UseEndTime    int64   `json:"useEndTime"`
+	UseStartTime  int64   `json:"useStartTime"`
+}
+
+type ImageInfo struct {
+	ImageList  []ImageList `json:"imageList"`
+	WhiteImage string      `json:"whiteImage"`
+}
+
+type ImageList struct {
+	URL string `json:"url"`
+}
+
+type PriceInfo struct {
+	LowestCouponPrice float64 `json:"lowestCouponPrice"`
+	LowestPrice       float64 `json:"lowestPrice"`
+	LowestPriceType   int64   `json:"lowestPriceType"`
+	Price             float64 `json:"price"`
+}
+
+type ShopInfo struct {
+	AfsFactorScoreRankGrade       string  `json:"afsFactorScoreRankGrade"`
+	AfterServiceScore             string  `json:"afterServiceScore"`
+	CommentFactorScoreRankGrade   string  `json:"commentFactorScoreRankGrade"`
+	LogisticsFactorScoreRankGrade string  `json:"logisticsFactorScoreRankGrade"`
+	LogisticsLvyueScore           string  `json:"logisticsLvyueScore"`
+	ScoreRankRate                 string  `json:"scoreRankRate"`
+	ShopID                        int64   `json:"shopId"`
+	ShopLabel                     string  `json:"shopLabel"`
+	ShopLevel                     float64 `json:"shopLevel"`
+	ShopName                      string  `json:"shopName"`
+	UserEvaluateScore             string  `json:"userEvaluateScore"`
 }
