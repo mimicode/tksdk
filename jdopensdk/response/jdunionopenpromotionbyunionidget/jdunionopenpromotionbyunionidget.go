5f92f382e79c266b702635e4376f1dbc8a0be138
Your Name <your@email.com>
2026-06-16 17:29:45 +0800

    完善 jdunionopenpromotionbyunionidget 响应

--- jdopensdk/response/jdunionopenpromotionbyunionidget/jdunionopenpromotionbyunionidget.go
+++ jdopensdk/response/jdunionopenpromotionbyunionidget/jdunionopenpromotionbyunionidget.go
@@ -48,7 +48,16 @@ type Responce struct {
 
 // QueryResult 具体内容
 type QueryResult struct {
-	Code      int64  `json:"code"`
-	Message   string `json:"message"`
-	RequestID string `json:"requestId"`
+	Code      int64              `json:"code"`
+	Message   string             `json:"message"`
+	RequestID string             `json:"requestId"`
+	Data      *PromotionCodeResp `json:"data"`
+}
+
+type PromotionCodeResp struct {
+	ShortURL        string `json:"shortURL"`        //生成的推广目标链接，以短链接形式，有效期60天
+	ClickURL        string `json:"clickURL"`        //生成推广目标的长链，长期有效
+	JCommand        string `json:"jCommand"`        //需要权限申请，京口令（匹配到红包活动有效配置才会返回京口令）
+	JShortCommand   string `json:"jShortCommand"`   //需要权限申请，短口令
+	WeChatShortLink string `json:"weChatShortLink"` //微信小程序ShortLink，当weChatType入参1时为京小街ShortLink，当weChatType入参2时为京东购物或京东外卖ShortLink
 }
